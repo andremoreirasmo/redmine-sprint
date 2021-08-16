35b912481669ff6d17925b4fd6d6c3fc1b4ef632
André Luis <andreluismoreirasmo@gmail.com>
2021-08-16 20:53:31 -0300

    Iniciado dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
-import * as React from "react";
-import { useSelector, shallowEqual, useDispatch } from "react-redux";
-import "./styles.css";
+import Dashboard from './components/dashboard/dashboard';
 
-import { Article } from "./components/Article";
-import { AddArticle } from "./components/AddArticle";
-import { addArticle, removeArticle } from "./store/actionCreators";
-import { Dispatch } from "redux";
-
-const App: React.FC = () => {
-  const articles: readonly IArticle[] = useSelector(
-    (state: ArticleState) => state.articles,
-    shallowEqual
-  );
-
-  const dispatch: Dispatch<any> = useDispatch();
-
-  const saveArticle = React.useCallback(
-    (article: IArticle) => dispatch(addArticle(article)),
-    [dispatch]
-  );
-
-// My blog: https://www.ibrahima-ndaw.com/
-
+export default function App() {
   return (
     <main>
       <h1>My Articles</h1>
-      <AddArticle saveArticle={saveArticle} />
-      {articles.map((article: IArticle) => (
-        <Article
-          key={article.id}
-          article={article}
-          removeArticle={removeArticle}
-        />
-      ))}
+      <Dashboard />
     </main>
   );
 };
-
-export default App;
