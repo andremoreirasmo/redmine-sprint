0dea976d89795e30d03845a760681804d0b973e9
Andre Luis Moreira <andreluismoreirasmo@gmail.com>
2021-09-20 21:53:12 -0300

    Doc: Configurado edit config/eslint/prettier

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://servicodados.ibge.gov.br/api/v1/'
-})
+  baseURL: "http://localhost:3333",
+});
 
 export default api;
