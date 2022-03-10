559c318e6ba2ae5b8aa5659ec57a9c072cc651c5
Andre Luis Moreira <andreluismoreirasmo@gmail.com>
2022-03-09 22:00:41 -0300

    Doc: Ajuste para desativar hihglight do autocomplete do chrome

--- src/pages/dashboard/styles.tsx
+++ src/pages/dashboard/styles.tsx
@@ -10,6 +10,16 @@ export const Logo = styled.img`
 
 export const Root = styled.div`
   display: flex;
+
+  /* Change the white to any color */
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    transition: background-color 5000s ease-in-out 0s;
+    -webkit-text-fill-color: ${props =>
+      props.theme.palette.text.primary + ' !important'};
+  }
 `;
 
 interface HeaderAppBarProps {
